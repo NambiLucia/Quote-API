373476512390b5e68b31069c2a593172ee56b53e
lucia nambi <nambilucia@gmail.com>
2024-08-09 09:30:43 +0300

    fix PUT error

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -2,68 +2,88 @@ const fs = require("node:fs");
 const path = require("path");
 const filePath = path.join(__dirname, "../Models/quotes.json");
 
+const getQuotes = (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Failed to get the Data");
+    }
+    res.send(data);
+  });
+};
 
-
-const getQuotes=(req, res) => {
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) {
-        return res.status(500).send("Failed to get the Data");
+const getQuotesById=(req,res)=>{
+  fs.readFile(filePath,"utf8",(err,data)=>{
+    if(err){
+      res.status(500).send("Failed to retrieve Data")
+    }
+      let quotes=JSON.parse(data);
+      let quoteId=req.params["id"];
+      if (quoteId >= 0 && quoteId < quotes.length) {
+        res.status(200).json(quotes[quoteId]); // Return the specific quote as JSON
+      } else {
+        res.status(404).send("Quote not found");
       }
-      res.send(data);
-    });
-  }
 
-  const createQuotes=(req, res) => {
-    const newQuote = req.body; // One created in postman
+
+  })
+
+
+
+
+}
+
+const createQuotes = (req, res) => {
+  const newQuote = req.body; // One created in postman
   // read to get existing quotes first
-    fs.readFile(filePath, "utf8", (err, data) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Failed to get the Data");
+    }
+
+    let oldQuotes = JSON.parse(data); //to be js obj for manipulation as array
+
+    oldQuotes.push(newQuote);
+
+    //write updated quotes array back to filepath
+
+    fs.writeFile(filePath, JSON.stringify(oldQuotes, null, 2), (err) => {
       if (err) {
-        return res.status(500).send("Failed to get the Data");
+        return res.status(500).send("Failed to save new quote");
       }
-  
-      let oldQuotes = JSON.parse(data);//to be js obj for manipulation as array
-  
-      oldQuotes.push(newQuote);
-  
-      //write updated quotes array back to filepath
-  
-      fs.writeFile(filePath, JSON.stringify(oldQuotes, null, 2), (err) => {
-        if (err) {
-          return res.status(500).send("Failed to save new quote");
-        }
-        res.send("Saved successfully");
-      
-      });
+      res.send("Saved successfully");
+    });
+  });
+};
+
+const updateQuotesById = (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Failed to Update the Data");
+    }
+    let quotes = JSON.parse(data); //parse JSON data into an array
+    let quoteId = parseInt(req.params["id"]); //get quote ID from req parameters
+    let theUpdatedQuote = req.body; //get updated quote from req body
+
+  //if quoteID is valid
+  if (quoteId >= 0 && quoteId < quotes.length) {
+    quotes[quoteId] = theUpdatedQuote; // Update the specific quote
+
+    fs.writeFile(filePath,JSON.stringify(quotes, null, 2), (err) => {
+      if (err) {
+        res.status(200).send(`Quote ID:${quoteId} Updated Successfully`);
+      }
+      res.status(404).send(`Quote ID:${quoteId} NOT updated`);
     });
   }
 
-  const updateQuotes=(req,res)=>{
-    fs.readFile(filePath,"utf8",(err,data) =>{
-      if(err){
+  
+  });
 
-        return res.status(500).send("Failed to Update the Data");
-      }
-      let quotes =JSON.parse(data);//parse JSON data into an array
-      let quoteId=req.params["id"]; //get quote ID from req parameters
-      let theUpdatedQuote=req.body;//get updated quote from req body
-     
-    })
+};
 
-    if (quoteId >= 0&& quoteId < quotes.length) {
-      quotes[quoteId] = theUpdatedQuote; // Update the specific quote
-
-    fs.writeFile(JSON.stringify(quotes,null,2),(err)=>{
-      if(err){
-        res.send(`Quote ID:${quoteId} Updated Successfully`)
-      }
-      res.status(200).send(`Quote ID:${quoteId} NOT updated`)
-    });
-
-
-  } }
-
-  module.exports={
-    createQuotes,
-    getQuotes,
-    updateQuotes
-  }
+module.exports = {
+  createQuotes,
+  getQuotes,
+  updateQuotesById,
+  getQuotesById
+};
