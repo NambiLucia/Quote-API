964ae0fdd9b5e55d749fe5aa74f47c7bc1625d3c
lucia nambi <nambilucia@gmail.com>
2024-08-09 17:33:55 +0300

    add request logs file

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const fs = require("node:fs");
 const quotesRouter = require("./routes/quotesRouter"); //can be any variable name
 const authorsRouter = require("./routes/authorsRouter");
+const path = require('path');
+
 
 const app = express();
 const PORT = 4800;
 
 app.use(express.json());
-app.use(morgan("dev"));
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'Logs', 'request_logs.txt'), { flags: 'a' })
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
 //middelware to direct endpoint requests to quotesRouter
 app.use("/quotes", quotesRouter);
 app.use("/authors", authorsRouter);
