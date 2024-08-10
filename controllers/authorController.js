edc882eb8b59e322fd2884d77af467e56da0d12c
lucia nambi <nambilucia@gmail.com>
2024-08-10 10:25:11 +0300

    add author controllers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,128 +2,51 @@ const fs = require("node:fs");
 const path = require("path");
 const filePath = path.join(__dirname, "../Models/authors.json");
 
-// Helper function to read the authors file
-const readAuthorsFile = (callback) => {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return callback(err);
-    }
-    try {
-      const authors = JSON.parse(data);
-      callback(null, authors);
-    } catch (parseErr) {
-      callback(parseErr);
-    }
-  });
-};
-
-// Helper function to write to the authors file
-const writeAuthorsFile = (authors, callback) => {
-  fs.writeFile(filePath, JSON.stringify(authors, null, 2), (err) => {
-    callback(err);
-  });
-};
-
-// Get all authors
 const getAuthors = (req, res) => {
-  readAuthorsFile((err, authors) => {
-    if (err) {
-      return res.status(500).send("Failed to retrieve the data");
-    }
-    res.json(authors);
-  });
+  const authors = JSON.parse(fs.readFileSync(filePath));
+  res.json(authors);
 };
 
-// Get author by ID
+
 const getAuthorsById = (req, res) => {
-  const authorId = parseInt(req.params.id);
-  readAuthorsFile((err, authors) => {
-    if (err) {
-      return res.status(500).send("Failed to retrieve the data");
-    }
-
-    const author = authors.find((a) => a.id === authorId);
-
-    if (author) {
-      return res.json(author);
-    } else {
-      return res.status(404).send("Author not found");
-    }
-  });
-};
-
-// Create a new author
-const createAuthors = (req, res) => {
-  const newAuthor = req.body;
-
-  readAuthorsFile((err, authors) => {
-    if (err) {
-      return res.status(500).send("Failed to retrieve the data");
-    }
-
-    authors.push(newAuthor);
-
-    writeAuthorsFile(authors, (err) => {
-      if (err) {
-        return res.status(500).send("Failed to save new author");
-      }
-      res.status(201).send("Saved successfully");
-    });
-  });
-};
-
-// Update author by ID
-const updateAuthorsById = (req, res) => {
-  const authorId = parseInt(req.params.id);
-  const updatedAuthor = req.body;
-
-  if (!updatedAuthor || Object.keys(updatedAuthor).length === 0) {
-    return res.status(400).send("Request body is missing or empty");
+  const authors = JSON.parse(fs.readFileSync(filePath));
+  const author = authors.find(a => a.id === parseInt(req.params.id));
+  if (author) {
+      res.json(author);
+  } else {
+      res.status(404).json({ message: "Author not found" });
   }
-
-  readAuthorsFile((err, authors) => {
-    if (err) {
-      return res.status(500).send("Failed to retrieve the data");
-    }
-
-    const authorIndex = authors.findIndex((a) => a.id === authorId);
-    if (authorIndex === -1) {
-      return res.status(404).send("Author not found");
-    }
-
-    authors[authorIndex] = updatedAuthor;
-
-    writeAuthorsFile(authors, (err) => {
-      if (err) {
-        return res.status(500).send(`authorId:${authorId} not updated`);
-      }
-      res.send(`authorID:${authorId} updated successfully`);
-    });
-  });
 };
 
-// Delete author by ID
+const createAuthors = (req, res) => {
+  const authors = JSON.parse(fs.readFileSync(filePath));
+  const newAuthor = { id: Date.now(), ...req.body };
+  authors.push(newAuthor);
+  fs.writeFileSync(filePath, JSON.stringify(authors));
+  res.status(201).json(newAuthor);
+};
+
+const updateAuthorsById = (req, res) => {
+  const authors = JSON.parse(fs.readFileSync(filePath));
+  const index = authors.findIndex(a => a.id === parseInt(req.params.id));
+  if (index !== -1) {
+      authors[index] = { id: authors[index].id, ...req.body };
+      fs.writeFileSync(filePath, JSON.stringify(authors));
+      res.json(authors[index]);
+  } else {
+      res.status(404).json({ message: "Author not found" });
+  }
+};
+
 const deleteAuthorsById = (req, res) => {
-  
-  let authors = JSON.parse(data);
-  let authorId = parseInt(req.params["id"]);
-  const authorToDelete= authors.find(element =>element.id === authorId);
-  const index =authors.indexOf(authorToDelete);
-  authors.splice(index,1);
-  
-  s.writeFile(filePath,JSON.stringify(authors, null, 2), (err) => {
-    if (err) {
-   
-     return res.status(404).send(`Author ID:${authorId} NOT updated`);
-    }
-    
-    
-  res.status(200).send(`Quote ID:${authorId} Updated Successfully`);
-
-  });
-  
-
+  const authors = JSON.parse(fs.readFileSync(filePath));
+  const filteredAuthors = authors.filter(a => a.id !== parseInt(req.params.id));
+  fs.writeFileSync(filePath, JSON.stringify(filteredAuthors));
+  res.status(204).send();
 };
+
+
+
 
 module.exports = {
   createAuthors,
