453e91e4bb9c2178052cf2cf70f0bfbdc8edc092
lucia nambi <nambilucia@gmail.com>
2024-08-11 11:13:40 +0300

    add changes to author router

--- routes/authorsRouter.js
+++ routes/authorsRouter.js
@@ -2,14 +2,13 @@ const express = require("express");
 //const path = require("path");
 //deal with quote requests
 const authorsRouter = express.Router();
-const { getAuthors, getAuthorsById, createAuthors, updateAuthorsById, deleteAuthorsById } = require('../controllers/authorController');
+const authorController = require("../controllers/authorController");
 
 //quote get requests
-authorsRouter.get("/", getAuthors);
-authorsRouter.get("/:id", getAuthorsById);
-authorsRouter.post("/", createAuthors);
-authorsRouter.put("/:id", updateAuthorsById);
-authorsRouter.delete("/:id", deleteAuthorsById);
-
+authorsRouter.get("/", authorController.getAuthors);
+authorsRouter.get("/:id", authorController.getAuthorsById);
+authorsRouter.post("/", authorController.createAuthors);
+authorsRouter.put("/:id", authorController.updateAuthorsById);
+authorsRouter.delete("/:id", authorController.deleteAuthorsById);
 
 module.exports = authorsRouter;
