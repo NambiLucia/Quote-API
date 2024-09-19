a9141fd17111eb4cf2ddc08a0fdf3800d5155ba9
lucia nambi <nambilucia@gmail.com>
2024-09-19 12:34:07 +0300

    fix test error

--- __tests__/token.test.js
+++ __tests__/token.test.js
@@ -23,8 +23,9 @@ describe("verify if validate token function works", () => {
       
       await validateToken(req, res, next);
 //when token is valid
-     // expect(next).toHaveBeenCalled();
-    //above line brings error
+      //expect(next).toHaveBeenCalled();
+      //expect(res.status).not.toHaveBeenCalled();
+      //expect(res.json).not.toHaveBeenCalled();
 
   });
 });
